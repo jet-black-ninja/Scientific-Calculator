da290694bcdb7f44251f7a2af8adfb5a698afc63
jet_black_ninja <pramodkmr958@gmail.com>
2024-08-17 00:16:15 +0530

    buttons done

--- src/main/java/ScientificCalculator.java
+++ src/main/java/ScientificCalculator.java
@@ -229,6 +229,7 @@ class ScientificCalculator {
         buttonPanel.add(button1);
 
         JButton button2 = new JButton("DEL");
+        button2.setFont(new Font("Calibri Light", Font.PLAIN, 17));
         button2.addActionListener(e -> {
             String s = textField.getText();
             if(s.length() > 1){
@@ -247,13 +248,14 @@ class ScientificCalculator {
                 expression = "";
             }
         });
+        buttonPanel.add(button2);
 
         //Button for constant PI
         JButton button3 = new JButton("<html><body><span>π</span></body></html>");
         button3.setFont(new Font("Calibri Light", Font.PLAIN, 17));
         button3.addActionListener(e -> {
             if(! "0".equals(expression)){
-                textField.setText(textField.getText() + Character.toString((char)960));
+                textField.setText(textField.getText() + (char) 960);
             } else {
                 textField.setText(Character.toString((char)960));
             }
@@ -352,7 +354,7 @@ class ScientificCalculator {
         button10.setFont(new Font("Calibri Light", Font.PLAIN, 17));
         button10.addActionListener(e -> {
             if(! "0".equals(textField.getText())) {
-                textField.setText(textField.getText()+Character.toString((char)8730));
+                textField.setText(textField.getText()+ (char) 8730);
             } else {
                 textField.setText(Character.toString((char)8730));
             }
@@ -439,11 +441,11 @@ class ScientificCalculator {
             }
             if(s.charAt(s.length()-1)=='-' || s.charAt(s.length()-1) == 'x' || s.charAt(s.length()-1) == '+'){
                 String newString = s.substring(0, s.length()-1);
-                textField.setText(newString + Character.toString((char)247));
+                textField.setText(newString + (char) 247);
                 expression = expression.substring(0, expression.length()-1);
                 expression+='/';
             }else if(s.charAt(s.length()-1 ) != (char)247){
-                textField.setText(s+Character.toString((char)247));
+                textField.setText(s+ (char) 247);
                 expression +='/';
             }else {
                 textField.setText(s);
@@ -467,7 +469,7 @@ class ScientificCalculator {
             dot = false;
         });
         buttonPanel.add(button16);
-
+        //number 4
         JButton button17 = new JButton("4");
         button17.setFont(new Font("Calibri Light", Font.PLAIN, 17));
         button17.setBackground(new Color(220,220,220));
@@ -486,7 +488,7 @@ class ScientificCalculator {
             dot = false;
         });
         buttonPanel.add(button17);
-
+        // number 5
         JButton button18 = new JButton("5");
         button18.setFont(new Font("Calibri Light", Font.PLAIN, 17));
         button18.setBackground(new Color(220,220,220));
@@ -505,7 +507,7 @@ class ScientificCalculator {
             dot = false;
         });
         buttonPanel.add(button18);
-
+        //number 6
         JButton button19 = new JButton("6");
         button19.setFont(new Font("Calibri Light", Font.PLAIN, 17));
         button19.setBackground(new Color(220,220,220));
@@ -524,7 +526,7 @@ class ScientificCalculator {
             dot = false;
         });
         buttonPanel.add(button19);
-
+        //multiplication button
         JButton button20 = new JButton("x");
         button20.setFont(new Font("Calibri Light", Font.PLAIN, 17));
         button20.addActionListener(e -> {
@@ -549,7 +551,200 @@ class ScientificCalculator {
             dot = false;
         });
         buttonPanel.add(button20);
+        //natural log button
+        JButton button21 = new JButton("ln");
+        button21.setFont(new Font("Calibri Light", Font.PLAIN, 17));
+        button21.addActionListener(e->{
+            if(! "0".equals(textField.getText())) {
+                textField.setText(textField.getText()+"ln(");
+            }else {
+                textField.setText("ln(");
+            }
+            expression+=",ln,(";
+            num = false;
+            dot = false;
+        });
+        buttonPanel.add(button21);
+        // number 1
+        JButton button22 = new JButton("1");
+        button22.setFont(new Font("Calibri Light", Font.PLAIN, 17));
+        button22.setBackground(new Color(220,220,220));
+        button22.addActionListener(e -> {
+            if(! "0".equals(textField.getText())) {
+                textField.setText(textField.getText()+"1");
+            }else{
+                textField.setText("1");
+            }
+            if(num){
+                expression+="1";
+            }else{
+                expression+=",1";
+            }
+        });
+        buttonPanel.add(button22);
+        //number 2
+        JButton button23 = new JButton("2");
+        button23.setFont(new Font("Calibri Light", Font.PLAIN, 17));
+        button23.setBackground(new Color(220,220,220));
+        button23.addActionListener(e -> {
+            if(! "0".equals(textField.getText())) {
+                textField.setText(textField.getText()+"2");
+            }else{
+                textField.setText("2");
+            }
+            if(num){
+                expression+="2";
+            }else{
+                expression+=",2";
+            }
+            num = true;
+            dot = false;
+        });
+        buttonPanel.add(button23);
 
+        JButton button24 = new JButton("3");
+        button24.setFont(new Font("Calibri Light", Font.PLAIN, 17));
+        button24.setBackground(new Color(220,220,220));
+        button24.addActionListener(e -> {
+            if(! "0".equals(textField.getText())) {
+                textField.setText(textField.getText()+"3");
+            }else{
+                textField.setText("3");
+            }
+            if(num){
+                expression+="3";
+            }else{
+                expression+=",3";
+            }
+        });
+        buttonPanel.add(button24);
+        //substation button
+        JButton button25 = new JButton("-");
+        button25.setFont(new Font("Calibri Light", Font.PLAIN, 17));
+        button25.addActionListener(e -> {
+            String s = textField.getText();
+            if(s.equals("0")) {
+                expression += "0";
+            }
+            if(s.charAt(s.length()-1) == '+' ) {
+                String newString = s.substring(0, s.length()-1);
+                newString += "-";
+                expression = expression.substring(0, expression.length()-1);
+                expression += "-";
+                textField.setText(newString);
+            }else if(s.charAt(s.length()-1) != '-'){
+                s += '-';
+                textField.setText(s);
+                expression += '-';
+            } else {
+                textField.setText(s);
+            }
+            num = false;
+            dot = false;
+        });
+        buttonPanel.add(button25);
+        //log button
+        JButton button26 = new JButton("<html><body><span>log<sub>10</span></body></html>");
+        button26.setFont(new Font("Calibri Light", Font.PLAIN, 17));
+        button26.addActionListener(e -> {
+            if(! "0".equals(textField.getText())) {
+                textField.setText(textField.getText()+"log(");
+            }else{
+                textField.setText("log(");
+            }
+            expression +=",log,(";
+            num = false;
+            dot = false;
+        });
+        buttonPanel.add(button26);
+        // decimal button
+        JButton button27 = new JButton(".");
+        button27.setFont(new Font("Calibri Light", Font.PLAIN, 17));
+        button27.addActionListener(e -> {
+            String s = textField.getText();
+            if(s.charAt(s.length()-1) != '.'){
+                if(num && !dot){
+                    expression += ".";
+                    s+= ".";
+                }else if(!num && !dot){
+                    expression += ",.";
+                    s+=".";
+                }
+            }
+            num = true;
+            dot = true;
+            textField.setText(s);
+        });
+        buttonPanel.add(button27);
+        //number 0
+        JButton button28 = new JButton("0");
+        button28.setFont(new Font("Calibri Light", Font.PLAIN, 17));
+        button28.setBackground(new Color(220,220,220));
+        button28.addActionListener(e -> {
+            String s = textField.getText();
+            if("0".equals(textField.getText())) {
+                textField.setText("0");
+            } else {
+                textField.setText(textField.getText()+"0");
+                if(num){
+                    expression += "0";
+                }else{
+                    expression +=",0";
+                }
+            }
+            num = true;
+        });
+        buttonPanel.add(button28);
+
+        JButton button29 = new JButton("=");
+        button29.setFont(new Font("Calibri Light", Font.PLAIN, 17));
+        button29.setBackground(Color.orange);
+        button29.addActionListener(e->{
+            calculateMain();
+                StringBuilder s= new StringBuilder();
+                token.remove(token.size()-1);
+                for(String i:token){
+                    switch (i) {
+                        case "/" -> s.append(Character.toString((char) 247));
+                        case "sqrt" -> s.append(Character.toString((char) 8730));
+                        case "pi" -> s.append(Character.toString((char) 960));
+                        default -> s.append(i);
+                    }
+                }
+                exprLabel.setText(s+"=");
+                System.out.println(token);
+                textField.setText(result);
+                expression = result;
+                dot = true;
+                num = true;
+                token.clear();
+
+        });
+        buttonPanel.add(button29);
+
+        JButton button30 = new JButton("+");
+        button30.setFont(new Font("Calibri Light", Font.PLAIN, 17));
+        button30.addActionListener(e -> {
+           String s = textField.getText();
+           if(s.equals("0")){
+               expression += "0";
+           }if(s.charAt(s.length()-1) =='-'|| s.charAt(s.length()-1)=='x' || s.charAt(s.length()-1) == (char)(247)){
+               String newString = s.substring(0, s.length()-1);
+               newString += "+";
+               textField.setText(newString);
+               expression = expression.substring(0, expression.length()-1);
+               expression +=",+";
+            }else if(s.charAt(s.length()-1) != '+'){
+               s +="+";
+               textField.setText(s);
+               expression +=",+";
+            }else {
+               textField.setText(s);
+            }
+           num = false;
+           dot = false;
+        });
+        buttonPanel.add(button30);
 
         frmCalculator.setBounds(200,100,400,500);
         frmCalculator.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
